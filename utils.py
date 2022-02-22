992035c5589b5f41324ee3035a58574ba40d186f
Alex Mboutchouang <mboutchouangalex+github@gmail.com>
2022-02-22 12:30:31 +0100

    fix bug in utils

--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ def text_to_dict(text: str) ->tuple:
     return tuple(text.replace("\n", "").split(" "))
 
 def tsv_to_dict(text: str) ->tuple:
-    idx = text.replace("\n", "").index(" ")
-    return (text[:idx], text[idx+1:])
+    idx = text.replace("\n", "").split("\t")
+    return (idx[0], idx[1])
 
 def load_txt_file(path: str) ->list:
     with open(path, "r", encoding="utf-8") as fp:
